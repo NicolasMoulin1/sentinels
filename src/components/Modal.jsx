5aedaca3b6ae44ba81986abbaf7751d338f90679
Nicolas Moulin <nicolas.moulin104@gmail.com>
2025-09-17 11:25:18 +0200

    supression de quelque ligne de code en commentaire

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,3 @@
-/* import React from "react";
-import "./Modal.css";
-
-export default function Modal({ title, children, onClose }) {
-  return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div
-        className="modal"
-        onClick={(e) => e.stopPropagation()} // Empêche fermeture si clic dans la modale
-      >
-        {title && <h2>{title}</h2>}
-        <div className="modal-body">{children}</div>
-
-        <div className="modal-actions">
-          <button className="btn secondary" onClick={onClose}>
-            Fermer
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-} */
-
 import React from "react";
 import SuperHero from "../assets/Images/SuperHero.jpg";
 import "./Modal.css";
