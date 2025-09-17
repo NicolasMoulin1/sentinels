55ec6a79ca5bcc0535197fdbcf4e59e5c28b9f54
Nicolas Moulin <nicolas.moulin104@gmail.com>
2025-09-17 10:00:35 +0200

    ajout du style pour la modale de l'agenda super hero

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import "./Modal.css";
 
 export default function Modal({ title, children, onClose }) {
@@ -13,6 +13,33 @@ export default function Modal({ title, children, onClose }) {
 
         <div className="modal-actions">
           <button className="btn secondary" onClick={onClose}>
+            Fermer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+} */
+
+import React from "react";
+import SuperHero from "../assets/Images/SuperHero.jpg";
+import "./Modal.css";
+
+export default function Modal({ title, children, onClose }) {
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal"
+        style={{
+          background: `linear-gradient(135deg, rgba(255,255,255,0.1), rgba(244,244,249,0.85)), url(${SuperHero}) center/cover no-repeat`,
+        }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {title && <h2> {title}</h2>}
+        <div className="modal-body">{children}</div>
+
+        <div className="modal-actions">
+          <button className="btn-secondary" onClick={onClose}>
             Fermer
           </button>
         </div>
